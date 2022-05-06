378be281ae384ae8d5dd584465d488676dbc8052
Dean Merchant <dean.merchant@kairostech.io>
2022-05-06 09:49:22 +0100

    1.0.3

--- expo-leaflet/ExpoLeaflet.tsx
+++ expo-leaflet/ExpoLeaflet.tsx
@@ -1,11 +1,11 @@
-import { Asset } from 'expo-asset'
-import * as FileSystem from 'expo-file-system'
-import isEqual from 'lodash.isequal'
-import React, { useEffect, useRef, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
-import { WebView } from 'react-native-webview'
-import { ExpoLeafletProps, LeafletMapProps } from './web/src/ExpoLeaflet.types'
-import { LeafletWebViewEvent } from './web/src/model'
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import isEqual from "lodash.isequal";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+import { ExpoLeafletProps, LeafletMapProps } from "./web/src/ExpoLeaflet.types";
+import { LeafletWebViewEvent } from "./web/src/model";
 
 export const ExpoLeaflet = ({
   backgroundColor,
@@ -14,77 +14,79 @@ export const ExpoLeaflet = ({
   onMapLoad,
   ...rest
 }: ExpoLeafletProps) => {
-  const mapProps: LeafletMapProps = rest
-  const webViewRef = useRef<WebView>(null!)
-  const [webViewContent, setWebviewContent] = useState<string | null>()
-  const [isLoadingHtmlFile, setLoadingHtmlFile] = useState(true)
-  const [isWebviewReady, setWebviewReady] = useState(false)
-  const previousPropsRef = useRef<Partial<LeafletMapProps>>({})
+  const mapProps: LeafletMapProps = rest;
+  const webViewRef = useRef<WebView>(null!);
+  const [webViewContent, setWebviewContent] = useState<string | null>();
+  const [isLoadingHtmlFile, setLoadingHtmlFile] = useState(true);
+  const [isWebviewReady, setWebviewReady] = useState(false);
+  const previousPropsRef = useRef<Partial<LeafletMapProps>>({});
   useEffect(() => {
     let isNotCancelled = true;
 
     const loadHtmlFile = async () => {
       try {
-        const path = require(`./assets/index.html`)
-        const htmlFile: Asset = await Asset.fromModule(path)
-        await htmlFile.downloadAsync()
+        const path = require(`./assets/index.html`);
+        const htmlFile: Asset = await Asset.fromModule(path);
+        await htmlFile.downloadAsync();
         const webviewContent: string = await FileSystem.readAsStringAsync(
-          htmlFile.localUri!,
-        )
+          htmlFile.localUri!
+        );
         if (isNotCancelled) {
-          setWebviewContent(webviewContent)
+          setWebviewContent(webviewContent);
           onMessage({
-            tag: 'DebugMessage',
-            message: 'WebView content loaded',
-          })
+            tag: "DebugMessage",
+            message: "WebView content loaded",
+          });
         }
       } catch (error) {
         if (isNotCancelled) {
-          onMessage({ tag: 'Error', error })
+          onMessage({ tag: "Error", error });
         }
       }
-    }
+    };
 
-    loadHtmlFile().catch(() => {})
+    loadHtmlFile().catch(() => {});
 
     return () => {
       isNotCancelled = false;
-    }
-  }, [])
+    };
+  }, []);
 
   useEffect(() => {
     if (!isWebviewReady) {
-      return
+      return;
     }
-    const previousProps = previousPropsRef.current
-    const newMapProps: Partial<LeafletMapProps> = {}
+    const previousProps = previousPropsRef.current;
+    const newMapProps: Partial<LeafletMapProps> = {};
     if (!isEqual(mapProps.mapCenterPosition, previousProps.mapCenterPosition)) {
-      newMapProps.mapCenterPosition = mapProps.mapCenterPosition
+      newMapProps.mapCenterPosition = mapProps.mapCenterPosition;
     }
     if (!isEqual(mapProps.mapLayers, previousProps.mapLayers)) {
-      newMapProps.mapLayers = mapProps.mapLayers
+      newMapProps.mapLayers = mapProps.mapLayers;
     }
     if (!isEqual(mapProps.mapMarkers, previousProps.mapMarkers)) {
-      newMapProps.mapMarkers = mapProps.mapMarkers
+      newMapProps.mapMarkers = mapProps.mapMarkers;
     }
     if (!isEqual(mapProps.mapOptions, previousProps.mapOptions)) {
-      newMapProps.mapOptions = mapProps.mapOptions
+      newMapProps.mapOptions = mapProps.mapOptions;
     }
     if (!isEqual(mapProps.mapShapes, previousProps.mapShapes)) {
-      newMapProps.mapShapes = mapProps.mapShapes
+      newMapProps.mapShapes = mapProps.mapShapes;
     }
     if (mapProps.maxZoom !== previousProps.maxZoom) {
-      newMapProps.maxZoom = mapProps.maxZoom
+      newMapProps.maxZoom = mapProps.maxZoom;
     }
     if (mapProps.zoom !== previousProps.zoom) {
-      newMapProps.zoom = mapProps.zoom
+      newMapProps.zoom = mapProps.zoom;
     }
     previousPropsRef.current = {
       ...previousProps,
       ...mapProps,
-    }
-    const payload = JSON.stringify(newMapProps)
-    webViewRef.current?.injectJavaScript(`window.postMessage(${payload}, '*');`)
+    };
+    const payload = JSON.stringify(newMapProps);
+    webViewRef.current?.injectJavaScript(
+      `window.postMessage(${payload}, '*');`
+    );
   }, [
     isWebviewReady,
     mapProps.mapCenterPosition,
@@ -94,15 +96,15 @@ export const ExpoLeaflet = ({
     mapProps.mapShapes,
     mapProps.maxZoom,
     mapProps.zoom,
-  ])
+  ]);
 
   return (
     <View
       style={[
         StyleSheet.absoluteFill,
         {
-          backgroundColor: backgroundColor ?? 'white',
-          position: 'relative',
+          backgroundColor: backgroundColor ?? "white",
+          position: "relative",
           flex: 1,
         },
       ]}
@@ -113,41 +115,41 @@ export const ExpoLeaflet = ({
           allowUniversalAccessFromFileURLs={true}
           allowFileAccessFromFileURLs={true}
           containerStyle={{
-            height: '100%',
-            width: '100%',
+            height: "100%",
+            width: "100%",
           }}
           domStorageEnabled={true}
           javaScriptEnabled={true}
           ref={webViewRef}
           onLoadEnd={() => {
-            setLoadingHtmlFile(false)
+            setLoadingHtmlFile(false);
           }}
           onLoadStart={() => {
-            setLoadingHtmlFile(true)
+            setLoadingHtmlFile(true);
           }}
           onMessage={(event) => {
             if (event && event.nativeEvent && event.nativeEvent.data) {
               try {
                 const message: LeafletWebViewEvent = JSON.parse(
-                  event.nativeEvent.data,
-                )
-                if (message.tag === 'MapComponentMounted') {
-                  setWebviewReady(true)
-                  onMapLoad?.()
+                  event.nativeEvent.data
+                );
+                if (message.tag === "MapComponentMounted") {
+                  setWebviewReady(true);
+                  onMapLoad?.();
                 }
-                onMessage(message)
+                onMessage(message);
               } catch (error) {
                 onMessage({
-                  tag: 'Error',
+                  tag: "Error",
                   error: { error, data: event.nativeEvent.data },
-                })
+                });
               }
             }
           }}
           onError={(error: any) => {
-            onMessage({ tag: 'Error', error })
+            onMessage({ tag: "Error", error });
           }}
-          originWhitelist={['*']}
+          originWhitelist={["*"]}
           renderLoading={loadingIndicator}
           source={{ html: webViewContent }}
           startInLoadingState={true}
@@ -159,5 +161,5 @@ export const ExpoLeaflet = ({
         </View>
       )}
     </View>
-  )
-}
+  );
+};
